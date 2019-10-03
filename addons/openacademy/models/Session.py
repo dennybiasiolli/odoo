f9da6af414b0a072fec2a139bbc3b2128ec9cac7
Denny Biasiolli <denny.biasiolli@gmail.com>
2019-10-03 13:56:49 +0200

    addons(openacademy): adding relations to Course and Session models

--- addons/openacademy/models/Session.py
+++ addons/openacademy/models/Session.py
@@ -8,3 +8,17 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+
+    instructor_id = fields.Many2one(
+        'res.partner',
+        ondelete='set null',
+        string='Instructor',
+        index=True,
+    )
+    course_id = fields.Many2one(
+        'openacademy.course',
+        ondelete='cascade',
+        string="Course",
+        required=True,
+        index=True,
+    )
